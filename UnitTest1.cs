9e06bc3d2c3ff41300d84e32999ad68604c5de85
kingradas <98946296+RadimPokorny@users.noreply.github.com>
2023-03-23 15:29:50 +0100

    commit1

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,98 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace UnitTest1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        enum Face
         {
+            Velkynos,
+            Usoplesk,
+            Makeup
         }
+
+        [TestMethod]
+        public void Test_LengthyName()
+        {
+            string name = "Oškyyyyyyyyyyyyyyyyyyy";
+            Assert.IsFalse(name.Length >= 10);
+        }
+        [TestMethod]
+        public void Test_Level()
+        {
+            int level = 6;
+            Assert.IsFalse(level > 1);
+        }
+        [TestMethod]
+
+        public void Test_Class()
+        {
+            string input = "Lyceista";
+            string[] pole = { "Kouzelník", "Berserker", "Inženýr", "Cizák" };
+            bool itiswhatitis = false;
+            for (int i = 0; i > pole.Length; i++)
+            {
+                if (input == pole[i])
+                {
+                    itiswhatitis = true;
+                }
+            }
+            Assert.AreSame(itiswhatitis, true);
+        }
+
+        [TestMethod]
+        public void Test_IsCharacterBoss()
+        {
+            bool bossStatus = false;
+
+            Assert.IsTrue(bossStatus == true);
+
+
+        }
+        [TestMethod]
+        public void Test_LotOfXp()
+        {
+            int xpAmount = 122;
+
+            Assert.IsFalse(xpAmount > 100);
+
+        }
+
+        [TestMethod]
+
+        public void Test_ExpectedTrait()
+        {
+            Assert.AreEqual("Klaun", Face.Makeup);
+        }
+
+        [TestMethod]
+        public void Test_OriginalPositionY()
+        {
+            int posY = -155;
+
+            Assert.IsTrue(posY == 0);
+
+        }
+        [TestMethod]
+        public void Test_OriginalPositionX()
+        {
+            int posX = -155;
+
+            Assert.IsTrue(posX == 0);
+
+        }
+
+
     }
+    
 }
+    
+    
+
+
+
